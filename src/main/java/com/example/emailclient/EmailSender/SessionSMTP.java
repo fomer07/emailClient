a8e1b9b2fd8e99ddb45dc6905db6a198bcb3e339
a1 <omerf.ay7@gmail.com>
2022-02-04 20:34:25 +0300

    session creation for SMTP protocol

---
+++ src/main/java/com/example/emailclient/EmailSender/SessionSMTP.java
@@ -0,0 +1,41 @@
+package com.example.emailclient.EmailSender;
+
+import com.example.emailclient.services.SessionService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import java.util.Properties;
+
+@Component
+public class SessionSMTP implements SessionService {
+    @Value("${mail.sender.smtp}")
+    String mailSmtpHost;
+    @Value("${mail.sender.address}")
+    String senderMailAddress;
+    @Value("${mail.sender.password}")
+    String senderMailPassword;
+    @Value("${mail.sender.port}")
+    Integer port;
+    @Override
+    public Session createAuthenticatedSession() {
+        Properties properties = new Properties();
+        properties.put("mail.transport.protocol","smtp");
+        properties.put("mail.smtp.host",mailSmtpHost);
+        properties.put("mail.smtp.port",port);
+        properties.put("mail.smtp.starttls.enable","true");
+        properties.put("mail.smtp.auth","true");
+        properties.put("mail.smtp.timeout","20000");
+        properties.put("mail.smtp.connectiontimeout","20000");
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(senderMailAddress,senderMailPassword);
+            }
+        });
+        session.setDebug(true);
+        return session;
+    }
+}
