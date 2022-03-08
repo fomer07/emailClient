a3289c11a54814997c3bc4c8001a36df7e4cabe7
a1 <omerf.ay7@gmail.com>
2022-03-08 15:49:56 +0300

    method addition

--- src/main/java/com/example/emailclient/services/FolderService.java
+++ src/main/java/com/example/emailclient/services/FolderService.java
@@ -2,11 +2,13 @@ package com.example.emailclient.services;
 
 import com.example.emailclient.model.FolderMessageInfo;
 
+import javax.mail.Folder;
 import javax.mail.Message;
+import java.util.Optional;
 
 public interface FolderService {
-    void getFolderInstance(String folderName);
-    void closeFolder();
-    FolderMessageInfo fetchFolderInformation();
-    Message[] getMessages();
+    Optional<Folder> getFolderInstance(String folderName);
+    FolderMessageInfo fetchFolderInformation(String folderName);
+    Message[] getMessages(String folderName);
+    Folder[] getFolderList();
 }
