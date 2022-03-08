b31417c4d0892459f7228cfb53c59a53d29ce703
a1 <omerf.ay7@gmail.com>
2022-03-08 15:31:05 +0300

    returned Optional instead of null in connectStore

--- src/main/java/com/example/emailclient/EmailCheck/StoreIMAP.java
+++ src/main/java/com/example/emailclient/EmailCheck/StoreIMAP.java
@@ -5,42 +5,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.mail.MessagingException;
-import javax.mail.NoSuchProviderException;
 import javax.mail.Store;
+import java.util.Optional;
 
 @Component
 public class StoreIMAP implements StoreService {
 
     private final SessionIMAP sessionIMAP;
-    private Store store;
+
 
     @Autowired
     public StoreIMAP(SessionIMAP sessionIMAP) {
         this.sessionIMAP = sessionIMAP;
     }
 
+
     @Override
-    public Store connectStore(){
+    public Optional<Store> connectStore(){
         try {
-            this.store = sessionIMAP.createAuthenticatedSession().getStore();
+            Store store = sessionIMAP.createAuthenticatedSession().getStore();
             store.connect();
+            return Optional.of(store);
         } catch (MessagingException e) {
             e.printStackTrace();
         }
-        return this.store;
+        return Optional.empty();
     }
-
-    @Override
-    public void logoutStore(){
-        try {
-            this.store.close();
-        } catch (MessagingException e) {
-            e.printStackTrace();
-        }
-    }
-
-
-
-
 
 }
