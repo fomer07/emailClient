d867c2ef40b4bd0cf0309f4bf97179e751aaf905
a1 <omerf.ay7@gmail.com>
2022-03-11 18:50:31 +0300

    lombok annotation added

--- src/main/java/com/example/emailclient/model/SimpleEmailEntity.java
+++ src/main/java/com/example/emailclient/model/SimpleEmailEntity.java
@@ -1,7 +1,13 @@
 package com.example.emailclient.model;
 
-import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
 
+
+@AllArgsConstructor
+@Getter
+@ToString
 public class SimpleEmailEntity {
 
     /**
@@ -9,11 +15,8 @@ public class SimpleEmailEntity {
      * address syntax is of the form "user@host.domain" or "Personal Name <user@host.domain>"
      */
     private String toAddresses;
-
     private String toCcAddresses;
-
     private String subject;
-
     private String messageBody;
 
     public SimpleEmailEntity(String toAddresses, String subject, String messageBody) {
@@ -22,37 +25,5 @@ public class SimpleEmailEntity {
         this.messageBody = messageBody;
     }
 
-    public SimpleEmailEntity(String toAddresses, String toCcAddresses, String subject, String messageBody) {
-        this.toAddresses = toAddresses;
-        this.toCcAddresses = toCcAddresses;
-        this.subject = subject;
-        this.messageBody = messageBody;
-    }
-
-    public String getToAddresses() {
-        return toAddresses;
-    }
-
-    public String getToCcAddresses() {
-        return toCcAddresses;
-    }
-
-    public String getSubject() {
-        return subject;
-    }
-
-    public String getMessageBody() {
-        return messageBody;
-    }
-
-    @Override
-    public String toString() {
-        return "SimpleEmailEntity{" +
-                "toAddresses='" + toAddresses + '\'' +
-                ", toCcAddresses='" + toCcAddresses + '\'' +
-                ", subject='" + subject + '\'' +
-                ", messageBody='" + messageBody + '\'' +
-                '}';
-    }
 
 }
